891acba4fb13f327687274599093ee43a791421c
LittleboyHarry <LittleboyHarry@users.noreply.github.com>
2020-07-31 18:20:07 +0800

    添加提问小帮手

---
+++ src/pages/tools/ask-better.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Layout from '@theme/Layout';
+import { Container } from 'reactstrap';
+
+export default () => <Layout>
+	<Container>
+		{/* todo
+		我先前尝试过使用搜索引擎解决问题（确定）
+		- 我会使用英文表达问题？(请你考虑使用非百度的优质搜索引擎)
+		考虑一下你的操作系统配置
+		- 我所提问的是计算机软硬件问题？
+		请问你高中毕业了吗？
+		*/}
+	</Container>
+</Layout>;
--- src/pages/tools/index.tsx
+++ src/pages/tools/index.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import Layout from '@theme/Layout';
-import { Container } from 'reactstrap';
+import { Container, Row, Col } from 'reactstrap';
+import Link from '@docusaurus/Link';
 
-export default () => <Layout>
-	<Container>
-		<h1 className="text-center m-5">神奇的百宝箱</h1>
-	</Container>
-</Layout>;
+export default () => (
+  <Layout>
+    <Container>
+      <h1 className="text-center m-5">神奇的百宝箱</h1>
+      <Row>
+        <Col sm={4}>
+          <Link to="tools/ask-better">技术提问</Link>
+        </Col>
+      </Row>
+    </Container>
+  </Layout>
+);
